8c1b5a58f4b9f5875e6908fdf6009286ac758827
initialrise <98199146+initialrise@users.noreply.github.com>
2024-04-26 15:28:45 +0545

    New task solution

---
+++ day12/tailwind2/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    colors: {
+      primary: "#253A67",
+      sec: "#DBE2EC99",
+    },
+    fontFamily: {
+      poppins: ["Poppins"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
